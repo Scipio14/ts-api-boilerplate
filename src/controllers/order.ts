28859a13711965bd6ed3bbb65ddb9a10e07c03e6
Federico Macías <fma.abogado@gmail.com>
2022-11-10 16:59:00 -0600

    Se añade ruta para subir archivos con multer

--- src/controllers/order.ts
+++ src/controllers/order.ts
@@ -1,11 +1,16 @@
 import { Request, Response } from "express";
-import dbConnect from "../config/mongo";
+import { JwtPayload } from "jsonwebtoken";
 import { handleHttp } from "../utils/error.handle";
 
-const getItems = (req: Request, res: Response) => {
+interface RequestExt extends Request {
+  user?: string | JwtPayload;
+}
+
+const getItems = (req: RequestExt, res: Response) => {
   try {
     res.send({
       data: "ESTO SOLO SE VE CON UN JWT VÁLIDO",
+      user: req.user,
     });
   } catch (e) {
     handleHttp(res, "ERROR_GET_ORDERS");
